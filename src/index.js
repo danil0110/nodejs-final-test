e80d43a62c72707c92429bb299addaaeea49c7fa
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-12-30 17:30:14 +0200

    Minor fix

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use('/users', require('./routes/users.routes'));
 app.use('/weather', require('./routes/weather.routes'));
+
+app.get('/', (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 const port = process.env.PORT || 4000;
 
